fbad676bd13a6df4fc7288f835aceb42d484d08a
Samuel Rettore <rettore@gmail.com>
2017-03-06 22:50:51 -0300

    Novos Arquivos

---
+++ Antenna_tracker/Controle.cpp
@@ -0,0 +1,8 @@
+#include "Arduino.h"
+#include "Controle.h"
+
+void Controle::Teste(int i){
+  Serial.print(" iiiiiiiiiii ");
+  Serial.print(i);
+}
+
---
+++ Antenna_tracker/Controle.h
@@ -0,0 +1,15 @@
+#ifndef Controle_h
+#define Controle_h
+
+#include "Arduino.h"
+
+class Controle {
+  public:
+   void Teste(int i);
+  private:
+   int _pino;
+  
+};
+
+#endif
+
