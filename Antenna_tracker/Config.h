482c5f032ea326632bdd2cb66b967243c7a08d8d
Samuel Rettore <rettore@gmail.com>
2017-03-07 08:07:33 -0300

    Adicionado arquivo de configuração.

---
+++ Antenna_tracker/Config.h
@@ -0,0 +1,49 @@
+/**
+ * Projeto desenvolvido por Samuel Rettore e Hugo Almeida
+ * Baseado no projeto https://youtu.be/aACTKziuDVE
+ * Antenna Tracker baseada em Arduino e RSSI 
+ * 
+ * ==== Configuração ===========
+ * 
+ */
+
+ 
+
+/*
+ * Configuracao RSSI
+ * Portas de leitura das anteas RSSI
+ * Niveis de RSSI para Calibração
+ */
+#define RSSI_direita 0
+#define RSSI_esquerda 1
+//#Future
+#define RSSI_vertical 2
+
+/**
+ * Valor usado para nao varrer todo montendo informações de leitura, somente
+ * quando diferencia de leitura maior que o parametro.
+ */
+#define DEADBAND 5
+
+/*
+ * Configura motores Servos .
+ * Portas Servo Motor
+ */
+#define SERVO_PAN 5
+#define SERVO_TILT 6
+
+/**
+ * Velocidade do servo
+ * Valores de 1 a 255
+ */
+#define SERVO_SPEED 50
+
+//Direção Normal 0, invertido -1.
+#define SERVO_DIRECTION     1
+
+/*
+ * Define Minimo e maximo do servo
+ */
+#define SERVO_MAX 180
+#define SERVO_MIN 13
+
